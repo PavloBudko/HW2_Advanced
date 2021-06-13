c1c12e25d4f36d03a2cb8d63c8ac24ecc58a77e2
PavloBudko <80714405+PavloBudko@users.noreply.github.com>
2021-06-13 20:14:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 let inputFirstNumber = prompt(`Введіть перше число:`);
 let inputSecondNumber = prompt(`Введіть друге число:`);
-let firstNumber = Number(Math.trunc(inputFirstNumber));
-let secondNumber = Number(Math.trunc(inputSecondNumber));
+const firstNumber = Number(Math.trunc(inputFirstNumber));
+const secondNumber = Number(Math.trunc(inputSecondNumber));
 
 while ((isNaN(firstNumber)) || isNaN(secondNumber)) {
     alert(`Спробуйте ще раз`);
     inputFirstNumber = prompt(`Введіть перше число:`);
     inputSecondNumber = prompt(`Введіть друге число:`);
-
-    firstNumber = Number(Math.trunc(inputFirstNumber));
-    secondNumber = Number(Math.trunc(inputSecondNumber));
 };
+
+const maximumNumber = Math.max(firstNumber, secondNumber);
+const minimumNumber = Math.min(firstNumber, secondNumber);
 
 const skipEvens = confirm(`Пропустим парні числа???`);
-
-let biggestNumber;
-let smallestNumber;
-if (firstNumber > secondNumber) {
-    biggestNumber = firstNumber;
-    smallestNumber = secondNumber;
-} else {
-    biggestNumber = secondNumber;
-    smallestNumber = firstNumber;
-};
 let sum = 0;
-for (smallestNumber; smallestNumber <= biggestNumber; smallestNumber++) {
-    if (skipEvens) {
-        if (smallestNumber % 2 === 0) continue;
+
+for (i = minimumNumber; i <= maximumNumber; i++) {
+    if (i % 2 === 0 && skipEvens) {
+        continue;
     }
-    sum = sum + smallestNumber;
-};
+    sum+=i;
+}
 
 alert(`Сума чисел = ` + sum);
 console.log(sum);
